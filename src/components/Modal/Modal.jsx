0d5dac8ca248c5e8afb48735558afba2e7515864
Alexey-Kushnir <al7787@gmail.com>
2023-03-08 14:18:42 +0200

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -4,10 +4,10 @@ import { Overlay, ModalWindow, Image } from './Modal.styled';
 
 export class Modal extends Component {
   componentDidMount() {
-    window.addEventListener('keydown', this.closeModal);
+    window.addEventListener('keydown', this.pressKeyDown);
   }
   componentWillUnmount() {
-    window.removeEventListener('keydown', this.closeModal);
+    window.removeEventListener('keydown', this.pressKeyDown);
   }
 
   backdropClick = e => {
@@ -22,14 +22,9 @@ export class Modal extends Component {
     }
   };
 
-  closeModal = e => {
-    this.backdropClick(e);
-    this.pressKeyDown(e);
-  };
-
   render() {
     return (
-      <Overlay onClick={this.closeModal}>
+      <Overlay onClick={this.backdropClick}>
         <ModalWindow>
           <Image src={this.props.img} alt="" />
         </ModalWindow>
