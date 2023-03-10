b71f5fa2929abd0f8f485875012a48f124d32906
Alexey-Kushnir <al7787@gmail.com>
2023-03-10 22:33:32 +0200

    Update services.js

--- src/components/services/services.js
+++ src/components/services/services.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-// import { abortController } from './../App';
 
 export const AxiosApiService = async (query, page, abortController) => {
   const url = `https://pixabay.com/api/?q=${query}&page=${page}&key=30802386-ea58133b3015dc71d1fd472a7&image_type=photo&orientation=horizontal&per_page=12`;
